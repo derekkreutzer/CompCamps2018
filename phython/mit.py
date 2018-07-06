4b7e8539a8118179ca81c7e76a9c4dd37c60e9c3
derek <derek.kreutzer4@gmail.com>
2018-07-05 18:21:14 -0600

    a big update

--- phython/mit.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class MIT:
-    """docstring for ."""
-    def __init__(self, name):
-        self.name=name
-        self.image = "images/" + self.name.lower() + ".jpg"
-        self.img = pygame.image.load(self.image)
-        self.img = pygame.transform.scale(self.img,(100,100))
--- phython/settings.py
+++
@@ -1,5 +0,0 @@
-class Settings:
-	title	      = "the MITS!!!"
-	width         = 800
-	height 	      = 600
-	frameRate     = 30
